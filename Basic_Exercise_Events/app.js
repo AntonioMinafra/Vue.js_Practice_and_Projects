a7c7835ed9432a3579eb9bda0434710c7c3ab554
Antonio <antoniominafra.dev@gmail.com>
2024-08-22 17:45:53 +0200

    terminated Basic_exercise_Events

--- Basic_Exercise_Events/app.js
+++ Basic_Exercise_Events/app.js
@@ -1,9 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      name: '',
+      counter: 5,
+      confirmedName: '',
     };
   },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('submitted!');
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    }
+  }
 });
 
 app.mount('#events');
