4b964ba31905c11f94b58ceb5af10637a643ad50
Antonio <antoniominafra.dev@gmail.com>
2024-09-03 19:08:44 +0200

    terminated Course_Project_01

--- Course_Project_01/app.js
+++ Course_Project_01/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             currentRound: 0,
             winner: null,
+            logMessages: [],
         }
     },
     methods: {
@@ -17,22 +18,26 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;
-            this.currentRound = 0
+            this.currentRound = 0;
+            this.logMessages = [];
         },
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -43,11 +48,19 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue;
             }
+            this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
         },
         surrender(){
             this.winner = 'monster';
-        }
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value,
+            });
+        },
     },
     computed: {
         monsterBarStyles(){
