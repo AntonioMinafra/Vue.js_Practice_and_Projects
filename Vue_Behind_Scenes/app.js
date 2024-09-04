e4739e5473391fcf900796a6044fef7b989b25b1
Antonio <antoniominafra.dev@gmail.com>
2024-09-04 14:24:10 +0200

    added another vue app in the same app.js

--- Vue_Behind_Scenes/app.js
+++ Vue_Behind_Scenes/app.js
@@ -14,5 +14,14 @@ const app = Vue.createApp({
     },
   },
 });
-
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  template: `<p>{{ favoriteMeal }}</p>`,
+  data() {
+    return {
+      favoriteMeal: 'Parmigiana'
+    }
+  },
+});
+app2.mount('#app2');
