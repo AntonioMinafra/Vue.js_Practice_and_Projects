f91651172a221ebbbb5ecaad1e28cba121d8cccb
Antonio <antoniominafra.dev@gmail.com>
2024-09-04 15:23:00 +0200

    added hooks example

--- Vue_Behind_Scenes/app.js
+++ Vue_Behind_Scenes/app.js
@@ -12,11 +12,41 @@ const app = Vue.createApp({
     setText() {
       /* this.message = this.currentUserInput; */
       this.message = this.$refs.userText.value;
+      /* ricordati che il refs è un oggetto con tantissime proprietà chiave:valore ovviamente a noi serve la proprietà value */
     },
+  },
+  /* serie di hooks che servono in generale ed esempio di come funzionano */
+  beforeCreate() {
+    console.log('ciao beforecreate');
+  },
+  created() {
+    console.log('ciao created');
+  },
+  beforeMount() {
+    console.log('beforemount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeupdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeUnmount() {
+    console.log('beforeunmount');
+  },
+  unmounted() {
+    console.log('unmounted');
   },
 });
 
 app.mount('#app');
+/* dopo 3 secondo l' app viene "smontata" */
+setTimeout( () => {
+  app.unmount();
+}, 3000)
 
 const app2 = Vue.createApp({
   template: `<p>{{ favoriteMeal }}</p>`,
